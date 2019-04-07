3e9a3c779aae61840e8a993269f69b843bb1e3db
Dan <headhertz88@gmail.com>
2019-04-07 14:12:34 -0600

    multiply and vowel count

---
+++ 8th kyu/multiply.js
@@ -0,0 +1,13 @@
+// The code does not execute properly. Try to figure out why.
+// https://www.codewars.com/kata/multiply/train/javascript
+
+function multiply(a, b) {
+	return a * b;
+}
+
+describe('Solution', function() {
+	it('should test for something', function() {
+		// Test.assertEquals(1 + 1, 2);
+		// assert.strictEqual(1 + 1, 2);
+	});
+});
