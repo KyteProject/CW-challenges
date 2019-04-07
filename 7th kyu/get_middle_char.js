e28828629257d7cb5f2995b816fc80f7b2a6ee39
Dan <headhertz88@gmail.com>
2019-04-07 14:12:34 -0600

    get middle char

---
+++ 7th kyu/get_middle_char.js
@@ -0,0 +1,32 @@
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// #Examples:
+// Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("A") should return "A"
+// #Input
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+// #Output
+// The middle character(s) of the word represented as a string.
+// https://www.codewars.com/kata/get-the-middle-character/train/javascript
+
+// solution(s)
+function getMiddle(s) {
+	const mid = s.length / 2;
+
+	return s.length % 2 ? s.charAt(mid) : s.substr(mid - 1, 2);
+}
+
+function getMiddle(s) {
+	return s.substring(Math.ceil(s.length / 2) - 1, Math.floor(s.length / 2) + 1);
+}
+
+// tests
+Test.describe('GetMiddle', function() {
+	Test.it('Tests', function() {
+		Test.assertEquals(getMiddle('test'), 'es');
+		Test.assertEquals(getMiddle('testing'), 't');
+		Test.assertEquals(getMiddle('middle'), 'dd');
+		Test.assertEquals(getMiddle('A'), 'A');
+	});
+});
