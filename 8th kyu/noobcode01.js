2576f6254b64645b74ee107bb13019f5f0bb0428
Dan <headhertz88@gmail.com>
2019-04-07 14:12:34 -0600

    noobcode 01

---
+++ 8th kyu/noobcode01.js
@@ -0,0 +1,31 @@
+// Write a function that rearranges an integer into its largest possible value.
+// superSize(123456) //654321
+// superSize(105) // 510
+// superSize(12) // 21
+// If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+// https://www.codewars.com/kata/noobcode-01-supersize-me-dot-dot-dot-or-rather-this-integer/train/javascript
+
+// solution
+function superSize(num) {
+	return parseInt(
+		num
+			.toString()
+			.split('')
+			.sort()
+			.reverse()
+			.join('')
+	);
+}
+
+// tests
+Test.describe('Basic tests', _ => {
+	Test.assertEquals(superSize(69), 96);
+	Test.assertEquals(superSize(513), 531);
+	Test.assertEquals(superSize(2017), 7210);
+	Test.assertEquals(superSize(414), 441);
+	Test.assertEquals(superSize(608719), 987610);
+	Test.assertEquals(superSize(123456789), 987654321);
+	Test.assertEquals(superSize(700000000001), 710000000000);
+	Test.assertEquals(superSize(666666), 666666);
+	Test.assertEquals(superSize(2), 2);
+});
