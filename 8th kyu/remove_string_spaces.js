627a54e13a1d46ecc630380bf37a0831b09da93c
Dan <headhertz88@gmail.com>
2019-04-07 14:12:34 -0600

    remove string spaces

---
+++ 8th kyu/remove_string_spaces.js
@@ -0,0 +1,12 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+// https://www.codewars.com/kata/remove-string-spaces/
+
+// solutions
+const noSpace = x => x.replace(/\s+/gi, '');
+
+// tests
+Test.describe('Example tests', _ => {
+	Test.assertEquals(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+	Test.assertEquals(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd');
+	Test.assertEquals(noSpace('8aaaaa dddd r     '), '8aaaaaddddr');
+});
