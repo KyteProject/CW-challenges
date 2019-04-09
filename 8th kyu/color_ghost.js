b43a26d081d3d00dd85fa2505562529fe852b38b
Dan <headhertz88@gmail.com>
2019-04-09 19:37:13 +0100

    colour ghost

---
+++ 8th kyu/color_ghost.js
@@ -0,0 +1,19 @@
+// Create a class Ghost
+// Ghost objects are instantiated without any arguments.
+// Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+
+// solution
+class Ghost {
+	constructor() {
+		this.colors = ['white', 'yellow', 'red', 'purple'];
+		this.color = this.colors[Math.floor(Math.random() * 4)];
+	}
+}
+
+// tests
+describe('Solution', function() {
+	it('should test for something', function() {
+		// Test.assertEquals(1 + 1, 2);
+		// assert.strictEqual(1 + 1, 2);
+	});
+});
